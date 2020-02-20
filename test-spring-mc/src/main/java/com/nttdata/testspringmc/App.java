4372cc97d5aaa092689605b7ba319e1750e35bb5
Arindam Mukherjee <arindam.mukherjee007@gmail.com>
2020-02-20 21:48:23 +0530

    22222 : US141 : New java file added

---
+++ test-spring-mc/src/main/java/com/nttdata/testspringmc/App.java
@@ -0,0 +1,11 @@
+package com.nttdata.testspringmc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World! Hello All !!!");
+	}
+}
